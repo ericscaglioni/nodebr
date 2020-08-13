417c5f3d0bf2c4d17be988067eae083bf5559acb
Eric Ferreira <eric.ferreira@luizalabs.com>
2020-08-13 17:24:38 -0300

    Ajustando codigo

--- 13-api-estrutura/src/routes/heroRoutes.js
+++ 13-api-estrutura/src/routes/heroRoutes.js
@@ -10,32 +10,8 @@ class HeroRoutes extends BaseRoute {
         return {
             method: 'GET',
             path: '/herois',
-            handler: (request, headers) => {
-                try {
-                    console.log('chegou', request)
-                    const {
-                        skip,
-                        limit,
-                        nome
-                    } = request.query
-
-                    console.log('skip', skip)
-                    console.log('limit', limit)
-
-                    let query = {}
-                    if (nome) {
-                        query.nome = nome
-                    }
-
-                    return this.db.read(
-                        query,
-                        parseInt(skip),
-                        parseInt(limit)
-                    );
-                } catch (error) {
-                    console.error('DEU RUIM', error);
-                    return 'Erro interno no servidor'
-                }
+            handler: (request) => {
+                return this.db.read();
             }
         }
     }
